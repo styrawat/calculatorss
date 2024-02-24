4ac0aa85a869ee50ecdb7b06df927ee448648436
satyam kumar rawat <satyamkumarrawat@satyams-MacBook-Air.local>
2024-02-24 23:18:17 +0530

    Initial commit

---
+++ lib/components/my_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+
+  final String title ;
+  final Color colors;
+  final VoidCallback onPress;
+
+  const MyButton({super.key,
+    required this.title,
+    this.colors = const Color(0xffa5a5a5),
+    required this.onPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: InkWell(
+          onTap: onPress,
+          child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: colors,
+              ),
+              child: Center(
+                child: Text(title,style: TextStyle(fontSize: 20,color: Colors.white),),
+              ),
+          ),
+        ),
+      ),
+    );
+  }
+}
